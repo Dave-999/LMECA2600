a6f92dadb075d4965a9f4e31678a9af369c2135f
ArnaudPaquet <arnaud.paquet@hotmail.com>
2016-11-24 17:55:30 +0100

    .

--- Reactor_model.m
+++ Reactor_model.m
@@ -1,13 +1,13 @@
 function m = Reactor_model(t_final,dt_plot,P_stable,PF_retarded)
 
-m_Utot = 1000; [kg]
+m_Utot = 1000; %[kg]
 m_U235 = m_Utot*0.07;
 m_U238 = m_Utot*0.93;
 
-n_initial = 1;
+n_initial = 10^10;
 
 %--------------------------------------------------
 
-
+Section_efficace('U235','Fission',30000,'DATABASE')
 
 end
