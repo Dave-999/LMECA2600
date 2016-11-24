39c79a43b00c83c520b6f34f57a4c7ed9f2e7023
ArnaudPaquet <arnaud.paquet@hotmail.com>
2016-11-24 14:50:49 +0100

    init

--- Reactor_model.m
+++ Reactor_model.m
@@ -1,5 +1,13 @@
 function m = Reactor_model(t_final,dt_plot,P_stable,PF_retarded)
 
+m_Utot = 1000; [kg]
+m_U235 = m_Utot*0.07;
+m_U238 = m_Utot*0.93;
+
+n_initial = 1;
+
+%--------------------------------------------------
+
 
 
 end
