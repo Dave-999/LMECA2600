d23f5f417f1ddfa1d4c107fda14897dc1c359590
Dave-999 <david.dispas@gmx.fr>
2016-12-05 21:55:57 +0100

    Vecteur neutrons thermiques
    Séparation par rapport au vecteur flux

--- Reactor_model.m
+++ Reactor_model.m
@@ -20,7 +20,7 @@ v_fast= 1000; %[m/s]
 n_thermal = 1e10; %Nombre de neutrons thermiques en t=0
 flux_thermal = n_thermal*v_thermal/V; %Flux de neutrons thermiques en t=0 [#/m^2.s]
 
-lambda_neutron = 0.5; %Lambda pour les fuites de neutrons et barres de controle
+lambda_neutron = 0.45; %Lambda pour les fuites de neutrons et barres de controle
 T_PF = 1;
 lambda_PF = log(2)/T_PF; %Lambda pour PF* = PF + n + 5 MeV
 
@@ -47,18 +47,21 @@ T = [0:dt_gen:t_final];
 Y = zeros(length(T),7); %U235,U238,U239,Np239,Pu239,PF*,PF
 Y(1,:) = [N_U235 N_U238 0 0 0 0 0]; %Quantites initiales
 N = zeros(length(T),1); %Flux de neutrons thermiques
-N(1,1) = flux_thermal; %Flux initial
+N(1,1) = n_thermal; %Nb de neutrons initial initial
+Phi = zeros(length(T),1);
+Phi(1,1) = flux_thermal;
 
 for i = 2:length(T)
-    Y(i,1) = Y(i-1,1) + (- Y(i-1,1)*fis_U235*N(i-1,1))*dt_gen; %U235
-    Y(i,2) = Y(i-1,2) + (- Y(i-1,2)*fis_U238*N(i-1,1) - Y(i-1,2)*cap_U238*N(i-1,1))*dt_gen; %U238
-    Y(i,3) = Y(i-1,3) + (Y(i-1,2)*cap_U238*N(i-1,1) - Y(i-1,3)*fis_U239*N(i-1,1) - Y(i-1,3)*log(2)/demi_U239)*dt_gen; %U239
-    Y(i,4) = Y(i-1,4) + (Y(i-1,3)*log(2)/demi_U239 - Y(i-1,4)*fis_Np239*N(i-1,1) - Y(i-1,4)*log(2)/demi_Np239)*dt_gen; %Np239
-    Y(i,5) = Y(i-1,5) + (Y(i-1,4)*log(2)/demi_Np239 - Y(i-1,5)*fis_Pu239*N(i-1,1))*dt_gen; %Pu239
-    Y(i,6) = Y(i-1,6) + (Y(i-1,1)*fis_U235*N(i-1,1) + Y(i-1,2)*fis_U238*N(i-1,1) + Y(i-1,3)*fis_U239*N(i-1,1) + Y(i-1,4)*fis_Np239*N(i-1,1) + Y(i-1,5)*fis_Pu239*N(i-1,1))*2*dt_gen - Y(i-1,6)*lambda_PF*dt_gen; %PF*
+    Y(i,1) = Y(i-1,1) + (- Y(i-1,1)*fis_U235*Phi(i-1,1))*dt_gen; %U235
+    Y(i,2) = Y(i-1,2) + (- Y(i-1,2)*fis_U238*Phi(i-1,1) - Y(i-1,2)*cap_U238*Phi(i-1,1))*dt_gen; %U238
+    Y(i,3) = Y(i-1,3) + (Y(i-1,2)*cap_U238*Phi(i-1,1) - Y(i-1,3)*fis_U239*Phi(i-1,1) - Y(i-1,3)*log(2)/demi_U239)*dt_gen; %U239
+    Y(i,4) = Y(i-1,4) + (Y(i-1,3)*log(2)/demi_U239 - Y(i-1,4)*fis_Np239*Phi(i-1,1) - Y(i-1,4)*log(2)/demi_Np239)*dt_gen; %Np239
+    Y(i,5) = Y(i-1,5) + (Y(i-1,4)*log(2)/demi_Np239 - Y(i-1,5)*fis_Pu239*Phi(i-1,1))*dt_gen; %Pu239
+    Y(i,6) = Y(i-1,6) + (Y(i-1,1)*fis_U235*Phi(i-1,1) + Y(i-1,2)*fis_U238*Phi(i-1,1) + Y(i-1,3)*fis_U239*Phi(i-1,1) + Y(i-1,4)*fis_Np239*Phi(i-1,1) + Y(i-1,5)*fis_Pu239*Phi(i-1,1))*2*dt_gen - Y(i-1,6)*lambda_PF*dt_gen; %PF*
     Y(i,7) = Y(i-1,7) + Y(i-1,6)*lambda_PF*dt_gen;
     
-    N(i,1) = N(i-1,1) + (Y(i-1,1)*fis_U235*N(i-1,1) + Y(i-1,2)*fis_U238*N(i-1,1) - Y(i-1,2)*cap_U238*N(i-1,1) + Y(i-1,3)*fis_U239*N(i-1,1) + Y(i-1,4)*fis_Np239*N(i-1,1) + Y(i-1,5)*fis_Pu239*N(i-1,1) + Y(i-1,6)*lambda_PF)*NA*(v_thermal/V)*dt_gen - N(i-1,1)*lambda_neutron*dt_gen; %Flux
+    N(i,1) = N(i-1,1) + (Y(i-1,1)*fis_U235*Phi(i-1,1) + Y(i-1,2)*fis_U238*Phi(i-1,1) - Y(i-1,2)*cap_U238*Phi(i-1,1) + Y(i-1,3)*fis_U239*Phi(i-1,1) + Y(i-1,4)*fis_Np239*Phi(i-1,1) + Y(i-1,5)*fis_Pu239*Phi(i-1,1) + Y(i-1,6)*lambda_PF)*NA*dt_gen - N(i-1,1)*lambda_neutron*dt_gen; %Nombres de neutrons (thermiques)
+    Phi(i,1) = N(i,1)*v_thermal/V; %flux thermique
 end
 
 %GRAPHES
@@ -82,7 +85,7 @@ legend('U235','U238','U239','Np239','Pu239','PF*','PF','Location','southeast');
 hold off;
 
 figure;
-loglog(T,N(:,1));
+loglog(T,Phi(:,1));
 xlabel('Temps [s]');
 ylabel('Flux de neutrons thermiques [#/m^2.s]');
 
